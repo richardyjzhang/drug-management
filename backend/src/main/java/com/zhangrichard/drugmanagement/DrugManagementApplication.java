c133b40ad7495375a4dfcb35aa79f1852c25ab29
Richard Zhang <richard.yujia.zhang@gmail.com>
2021-02-12 20:04:17 +0800

    Spring Boot初始化

---
+++ backend/src/main/java/com/zhangrichard/drugmanagement/DrugManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zhangrichard.drugmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zhangrichard/drugmanagement/DrugManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangrichard.drugmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
